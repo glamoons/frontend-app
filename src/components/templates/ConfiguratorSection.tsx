2a48c609bb3d3fd6357c67c5c44accd2ffe8b935
k_czech <kamczeh@gmail.com>
2024-02-07 11:28:47 +0100

    fixup! Merge branch 'feature/01-home-page--mobile-first' of github.com:glamoons/frontend-app into feature/01-home-page--mobile-first
    feat:small fixes to rwd screen

--- src/components/templates/ConfiguratorSection.tsx
+++ src/components/templates/ConfiguratorSection.tsx
@@ -5,9 +5,9 @@ import { SecondaryButton } from "../atoms/SecondaryButton";
 
 export const ConfiguratorSection = () => {
 	return (
-		<Section className="3xl:container 3xl:rounded-3xl overflow-hidden bg-primaryLight">
-			<div className="tablet:flex tablet:flex-row 3xl:px-0 container mx-auto">
-				<div className="tablet:w-1/2 tablet:min-h-max tablet:mr-[1.563rem] desktop:mr-[3.125rem] tablet:mt-0 tabletLg:items-start mt-[-50px] flex min-h-[390px] flex-col items-center justify-center space-y-6">
+		<Section className="overflow-hidden bg-primaryLight 3xl:container 3xl:rounded-3xl">
+			<div className="container mx-auto tablet:flex tablet:flex-row 3xl:px-0">
+				<div className="mt-[-50px] flex min-h-[390px] flex-col items-center justify-center space-y-6 tablet:mr-[1.563rem] tablet:mt-0 tablet:min-h-max tablet:w-1/2 tabletLg:items-start desktop:mr-[3.125rem]">
 					<SectionHeading
 						subHeading="konfigurator"
 						title="Dostosuj swój stolik"
@@ -15,29 +15,13 @@ export const ConfiguratorSection = () => {
 						containerStyle="text-left items-start"
 					/>
 					<SecondaryButton
-						className="tabletLg:w-auto w-full max-w-none"
+						className="w-full max-w-none tabletLg:w-auto"
 						href={"/"}
 					>
 						skonfiguruj
 					</SecondaryButton>
 				</div>
-				<div
-					className="
-				tablet:w-1/2
-				tablet:mx-0
-				tablet:-mt-[50px]
-				tablet:-mr-[1.563rem]
-				desktop:-mr-[calc(8.75rem)]
-				desktop:-mt-[100px]
-				desktop:-mb-[100px]
-				desktop:h-[476px]
-				desktop:w-[700px]
-				relative
-				-mx-[1.563rem]
-				mb-[-50px]
-				h-[390px]
-				2xl:h-[576px]"
-				>
+				<div className="relative -mx-[1.563rem] mb-[-50px] h-[390px] tablet:mx-0 tablet:-mr-[1.563rem] tablet:-mt-[50px] tablet:w-1/2 desktop:-mb-[100px] desktop:-mr-[calc(50%+8.75rem)] desktop:-mt-[100px] desktop:h-[476px] desktop:w-[700px] 2xl:h-[576px]">
 					<div className="absolute bottom-0 left-0 right-0 top-0 z-10 bg-black/15" />
 					<ResponsiveImage
 						mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1706681759/informational_mobile_nphus5.jpg"
