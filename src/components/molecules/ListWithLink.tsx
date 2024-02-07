eb3358f30e07cd2c53743888ca136ef97f40ded7
Kamil Czech <33509305+k-czech@users.noreply.github.com>
2024-02-07 11:30:05 +0100

    Feature/01 home page  mobile first (#4)
    * feat:added font, colors, metadata and logos to app
    
    * feat:added hero, hero title, header and image configuration
    
    * feat:added reusable components and section about products
    
    * feat:added sections to organisms and reusable atoms components with buttons, section, text, headings
    
    * fixup! feat:added sections to organisms and reusable atoms components with buttons, section, text, headings
    
    feat:fix for regular font
    
    * feat:changed project structure, added templates and moved components
    
    * feat:remove no needed tailwind plugin and changed category section heading
    
    * feat:added footer and some components
    
    * fixup! feat:added footer and some components
    
    feat:changed prettier config name and formatted class names
    
    * feat:added menu mobile
    
    * feat:added menu animation and some responsive dimmension
    
    * feat:improved section rwd
    
    * feat:added some rwd to desktop dimmension
    
    * feat:added favicons, images and rwd style for large screens
    
    * fixup! feat:added favicons, images and rwd style for large screens
    
    feat:remove unused import
    
    * feat:added improvments to html and style
    
    * fixup! Merge branch 'feature/01-home-page--mobile-first' of github.com:glamoons/frontend-app into feature/01-home-page--mobile-first
    
    feat:small fixes to rwd screen

--- src/components/molecules/ListWithLink.tsx
+++ src/components/molecules/ListWithLink.tsx
@@ -10,33 +10,7 @@ export const ListWithLink = ({
 	...props
 }: ListWithLinkProps & LinkProps) => {
 	return (
-		<li
-			className="
-            relative 
-            overflow-hidden
-            whitespace-nowrap 
-            text-base 
-            font-light
-            text-secondary
-            transition-all
-            duration-500
-            before:absolute
-            before:bottom-0
-            before:left-0 
-            before:z-[-1] 
-            before:h-full
-            before:w-1/2 
-            before:translate-x-[-100%] 
-            before:bg-primary 
-            before:opacity-0 
-            before:transition-all 
-            before:duration-500 
-            before:content-['']
-            hover:pl-3
-            before:hover:translate-x-0
-            before:hover:opacity-100
-"
-		>
+		<li className="relative overflow-hidden whitespace-nowrap text-base font-light text-secondary transition-all duration-500 before:absolute before:bottom-0 before:left-0 before:z-[-1] before:h-full before:w-1/2 before:translate-x-[-100%] before:bg-primary before:opacity-0 before:transition-all before:duration-500 before:content-[''] hover:pl-3 before:hover:translate-x-0 before:hover:opacity-100">
 			<Link href={href} {...props} className="block py-2">
 				{linkName}
 			</Link>
