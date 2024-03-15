2f4a719fabc910bb35dd93bc44e7359fae43d6c0
Kamil Czech <33509305+k-czech@users.noreply.github.com>
2024-03-15 10:32:21 +0100

    Feature/offer (#28)
    * feat:changed components for MDF base
    
    * feat:changed text and images in product page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,32 +20,32 @@ const ProductPage = () => {
 		<>
 			<ProductSection
 				subHeading="O produkcie"
-				title="Zaprojektuj swój mebel"
+				title="Unikalna Baza MDF"
 				description="A paragraph or two with information on your product/service or describes a problem your product/service is designed to solve."
-				mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1707385345/informational_mobile_nphus5_qfd61k.webp"
-				alt="Konfigurator"
-				desktopSrc="https://res.cloudinary.com/dstimijog/image/upload/v1708670729/about-product_l6xxbi.webp"
+				mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1710493907/product-image-mobile-01_bmbj4g.webp"
+				alt="Baza MDF"
+				desktopSrc="https://res.cloudinary.com/dstimijog/image/upload/v1710493587/about-product_l6xxbi.webp"
 				btnName="Zamów bazę MDF"
 				href="/"
 				right={false}
 			/>
 			<ProductSection
 				subHeading="Poczuj perfekcjonizm na własnej skórze"
-				title="Zegar na bazie MDF"
+				title="Oświetlenie LED"
 				description="A paragraph or two with information on your product/service or describes a problem your product/service is designed to solve. 
 				Provide your customers a story they would enjoy keeping in mind the objectives of your website. Pay special attention to the tone of voice. Try to win the customers’ trust by being positive."
-				mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1708931194/product-image-mobile-02_bmbj4g.webp"
-				alt="Stolik z solidnym blatem"
-				desktopSrc="https://res.cloudinary.com/dstimijog/image/upload/v1708931014/product-image-02_dqjkxf.webp"
+				mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1710493914/product-image-mobile-02_bmbj4g.webp"
+				alt="Baza MDF z oświetleniem LED"
+				desktopSrc="https://res.cloudinary.com/dstimijog/image/upload/v1710493580/product-image-02_dqjkxf.webp"
 				right
 			/>
 			<ProductSection
 				subHeading="Ekskluzywny wygląd"
 				title="Dopasuj bazę MDF do swojego produktu"
 				description="A paragraph or two with information on your product/service or describes a problem your product/service is designed to solve."
-				mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1708931194/product-image-mobile-03_qrgqqy.webp"
+				mobileSrc="https://res.cloudinary.com/dstimijog/image/upload/v1710493910/product-image-mobile-03_qrgqqy.webp"
 				alt="Dopasuj bazę MDF do swojego produktu"
-				desktopSrc="https://res.cloudinary.com/dstimijog/image/upload/v1708931014/product-image-03_mpj4r8.webp"
+				desktopSrc="https://res.cloudinary.com/dstimijog/image/upload/v1710494116/product-image-03_mpj4r8.webp"
 				right={false}
 				picItems={
 					<PlayButton
